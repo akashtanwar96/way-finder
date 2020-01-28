14f1e5f10810969917e32c87ac39049e343b6c6a
Akash Tanwar <AkashT@interrait.info>
2020-01-28 12:49:09 +0530

    very first commit for finder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finder';
+  address = [{ label: 'Gate 1', value: 1 },
+  { label: 'Gate 2', value: 2 },
+  { label: 'Reception', value: 3 },
+  { label: 'Parking', value: 4 },
+  { label: 'Canteen', value: 5 }];
+
+  from: number = this.address[0].value;
+  fromTitle: string = this.address[0].label;
+  toTitle: string = this.address[1].label;
+  to: number = this.address[1].value;
+  imgFlag = true;
+  imgURL : string =`./../assets/12.jpg`;
+  
+
+  onFromSelect(item: number,label: string) {
+    this.fromTitle = label
+    this.from = item;
+  }
+
+  onToSelect(item: number,label: string) {
+    this.toTitle = label
+    this.to = item;    
+  }
+
+  onSubmit() {
+    if(this.from == this.to) {
+      this.imgFlag = false;
+      console.log("No image as same source and destination");
+      
+    } else {
+      this.imgFlag = true;
+      this.imgURL = `./../assets/${this.from.toString().concat(this.to.toString())}.jpg`;
+      console.log(this.imgURL,this.imgFlag,'image should be availabe');
+    }
+  }
+    
+  getImg() {
+    if(isNullOrUndefined(this.from) && isNullOrUndefined(this.to)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
